fdcfe463c427a423ff39db9c17ed1b352668d1d5
Why070 <135810436+Why070@users.noreply.github.com>
2023-06-18 17:29:37 +0800

    Update run.py

--- llmtune/run.py
+++ llmtune/run.py
@@ -154,9 +154,6 @@ def finetune(args):
     from llmtune.executor import load_llm
   
     llm, tokenizer = load_llm(args.model, args.weights)
-
-    for name, param in model.named_parameters():
-        print(f"Name: {name}, size: {param.size()}")
     
     print("\033[1;31mMemory occupied during load_llm:\033[0m:")
     
