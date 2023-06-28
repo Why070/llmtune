75ef4628283a2b007158658c8e20ac16cf377dce
Why070 <135810436+Why070@users.noreply.github.com>
2023-06-28 10:55:02 +0800

    Update model.py

--- llmtune/llms/llama/model.py
+++ llmtune/llms/llama/model.py
@@ -8,7 +8,7 @@ from llmtune.engine.quant.converter import make_quant
 
 
 def get_gpu_memory_usage():
-    result = subprocess.run(['nvidia-smi'], capture_output=True, text=True)
+    result = subprocess.run(['nvidia-smi', '-i', '0', '-q', '-d', 'MEMORY'], capture_output=True, text=True)
     return result.stdout
 
 def get_memory():
